b9760f8e53ed90710ceee092ea725ebff02d01fa
Camilo Medrano <mm22108@ues.edu.sv>
2023-11-03 08:47:32 -0600

    Atributos y métodos de la clase Nodo.cs

--- AplicacionDeArbolesAVL/Nodo.cs
+++ AplicacionDeArbolesAVL/Nodo.cs
@@ -2,6 +2,37 @@ namespace AplicacionDeArbolesAVL
 {
     public class Nodo
     {
-        
+        private Simbolo simbolo;
+        private Nodo nodoIzquierdo;
+        private Nodo nodoDerecho;
+        private int altura;
+
+        public Nodo()
+        {
+        }
+
+        public Simbolo Simbolo
+        {
+            get => simbolo;
+            set => simbolo = value;
+        }
+
+        public Nodo NodoIzquierdo
+        {
+            get => nodoIzquierdo;
+            set => nodoIzquierdo = value;
+        }
+
+        public Nodo NodoDerecho
+        {
+            get => nodoDerecho;
+            set => nodoDerecho = value;
+        }
+
+        public int Altura
+        {
+            get => altura;
+            set => altura = value;
+        }
     }
 }
