74c4e82ed4cf4dc60c4b3852abc26881606fe780
Camilo Medrano <mm22108@ues.edu.sv>
2023-11-03 08:12:05 -0600

    Atributos y métodos de la clase Simbolo.cs

--- AplicacionDeArbolesAVL/Simbolo.cs
+++ AplicacionDeArbolesAVL/Simbolo.cs
@@ -2,6 +2,37 @@ namespace AplicacionDeArbolesAVL
 {
     public class Simbolo
     {
-        
+        private string nombre;
+        private string tipo;
+        private string ambito;
+        private string otrosDatos;
+
+        public Simbolo()
+        {
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+
+        public string Tipo
+        {
+            get => tipo;
+            set => tipo = value;
+        }
+
+        public string Ambito
+        {
+            get => ambito;
+            set => ambito = value;
+        }
+
+        public string OtrosDatos
+        {
+            get => otrosDatos;
+            set => otrosDatos = value;
+        }
     }
 }
