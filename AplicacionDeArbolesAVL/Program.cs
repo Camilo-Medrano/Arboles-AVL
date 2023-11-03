18922af8d1253ad5e940dc05898bdc082732fec4
Camilo Medrano <mm22108@ues.edu.sv>
2023-11-03 16:20:48 -0600

    Prueba de que el arbol funcione

--- AplicacionDeArbolesAVL/Program.cs
+++ AplicacionDeArbolesAVL/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace AplicacionDeArbolesAVL
+﻿using System;
+
+namespace AplicacionDeArbolesAVL
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            ArbolDeSimbolos arbol = new ArbolDeSimbolos();
+            Simbolo lineaCero = new Simbolo();
+            lineaCero.Nombre = "zapato";
+            lineaCero.Ambito = "private";
+            lineaCero.Tipo = "string";
+            lineaCero.OtrosDatos = "vans";
+
+            Simbolo lineaUno = new Simbolo();
+            lineaUno.Nombre = "precio";
+            lineaUno.Ambito = "private";
+            lineaUno.Tipo = "float";
+            lineaUno.OtrosDatos = "105.7";
+            
+            Simbolo lineaDos = new Simbolo();
+            lineaDos.Nombre = "talla";
+            lineaDos.Ambito = "private";
+            lineaDos.Tipo = "float";
+            lineaDos.OtrosDatos = "11.5";
+            
+            arbol.Raiz = arbol.Insertar(lineaCero, arbol.Raiz);
+            arbol.Raiz = arbol.Insertar(lineaUno, arbol.Raiz);
+            arbol.Raiz = arbol.Insertar(lineaDos, arbol.Raiz);
+            arbol.EnOrden(arbol.Raiz);
         }
     }
 }
