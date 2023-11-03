338331d5e0d5faac47ba36d688302c1e4c8c010c
Camilo Medrano <mm22108@ues.edu.sv>
2023-11-03 09:50:44 -0600

    Atributos y Métodos incompletos de AnalizadorSemantico.cs

--- AplicacionDeArbolesAVL/AnalizadorSemantico.cs
+++ AplicacionDeArbolesAVL/AnalizadorSemantico.cs
@@ -2,6 +2,17 @@ namespace AplicacionDeArbolesAVL
 {
     public class AnalizadorSemantico
     {
-        
+        private string codigo;
+        private ArbolDeSimbolos arbol = new ArbolDeSimbolos();
+
+        public void RecorrerCodigo()
+        {
+            
+        }
+
+        public bool VerificarExpresionRegular(string extracto)
+        {
+            return true;
+        }
     }
 }
